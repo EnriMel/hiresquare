92f8cf1fcd097f9ad227a846dea05ecb17401e41
Enrico Meluziis <enricomlz@hotmail.it>
2025-04-30 15:24:12 +0200

    creates a basic layout structure

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hyresquare</title>
+</head>
+
+<body>
+    <div>
+        <nav>
+            <div>logo</div>
+            <div>links</div>
+            <div>post a job</div>
+        </nav>
+        <main>{{ $slot }}</main>
+    </div>
+</body>
+
+</html>
