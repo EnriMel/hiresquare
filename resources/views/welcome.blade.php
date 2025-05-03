a438e8772a78982f7e6b2929beb69ad64c1378c2
Enrico Meluziis <enricomlz@hotmail.it>
2025-05-03 22:37:29 +0200

    completes top job card style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,25 @@
 <x-layout>
     <section>
-        <h3>Top Jobs</h3>
+        <h3 class="mb-6">Top Jobs</h3>
+
         <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
             <div class="self-start text-sm">Adventure TV</div>
-            <div>
+
+            <div class="py-8 font-bold">
                 <h3>Video Producer</h3>
                 <p>Full Time - From $60,000</p>
             </div>
-            <div>
-                <div>
-                    <a href="#">Tag</a>
-                    <a href="#">Tag</a>
-                    <a href="#">Tag</a>
-                </div>
 
-                <img src="http://placehold.co/42x42" alt="">
+            <div class="flex justify-between items-center mt-auto">
+                <div>
+                    <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                    <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                    <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                </div>
+                <img src="http://placehold.co/42x42" alt="" class="rounded-xl">
             </div>
+
         </div>
     </section>
 </x-layout>
