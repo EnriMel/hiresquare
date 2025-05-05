9eb460788ec1bb078139afbe88c03a57b11fc119
Enrico Meluziis <enricomlz@hotmail.it>
2025-05-05 14:25:18 +0200

    creates job-card-wide general shape

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <img src="http://placehold.co/42x42" alt="" class="rounded-xl">
+
+    <div>
+        <a href="#" class="self-start text-sm">Adventure TV</a>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
