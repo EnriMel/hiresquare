7872bff12f4cd4767623c000944a2a06cfdb5f2d
Enrico Meluziis <enricomlz@hotmail.it>
2025-06-01 17:35:52 +0200

    styling tag component

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,1 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl transition-colors duration-300 text-tiny">{{ $slot }}</a>
+<a href="#" class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl transition-colors font-bold duration-300 text-tiny">{{ $slot }}</a>
