70beb05c262c06e8843b07cb9eb04561e3c63a10
Enrico <e.meluziis@adhocweb.it>
2025-06-03 11:49:41 +0200

    creating a test - sets the first things

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Job;
+use App\Models\Employer;
+
+it('belongs to an employer', function () {
+    // AAA
+    // Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create();
+    
+
+    // Act
+    // Assert
+    
+    
+});
